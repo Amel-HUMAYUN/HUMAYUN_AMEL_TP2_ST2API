2e8777157617557ff00188de8750b9639b3cecb7
Amel-HUMAYUN <84172451+Amel-HUMAYUN@users.noreply.github.com>
2022-06-15 17:55:47 +0200

    Add files via upload

---
+++ HUMAYUN_AMEL_ST2API_TP2/HUMAYUN_AMEL_ST2API_TP2/Coord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HUMAYUN_AMEL_ST2API_TP2
+{
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+}
---
+++ HUMAYUN_AMEL_ST2API_TP2/HUMAYUN_AMEL_ST2API_TP2/Sys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HUMAYUN_AMEL_ST2API_TP2
+{
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public long sunrise { get; set; }
+        public long sunset { get; set; }
+    }
+}
